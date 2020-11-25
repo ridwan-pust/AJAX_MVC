90c3b88602efede5722693f930070460e1b8eb02
Ridwanul Islam <ridwan.pust@gmail.com>
2020-11-25 23:42:40 +0600

    Addfile

---
+++ MyselfController.cs
@@ -0,0 +1,40 @@
+﻿using AjaxCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxCRUD.Controllers
+{
+    public class MyselfController : Controller
+    {
+        private readonly Entities db = new Entities();
+
+        // GET: Myself
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            var obj=db.Students.ToList();
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            db.Students.Add(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StudentsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AjaxCRUD.Models;
+
+namespace AjaxCRUD.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly Entities db = new Entities();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult List()
+        {
+            return View(db.Students.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/CreateOrEdit/
+        // GET: Students/CreateOrEdit/5
+
+        public ActionResult CreateOrEdit(int? id)
+        {
+            if (id == null)
+            {
+                return View(new Student() { }) ;
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/CreateOrEdit/
+        // POST: Students/CreateOrEdit/1
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateOrEdit(int id,[Bind(Include = "ID,Name,Age")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                if(id==0)
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                }
+
+ 
+                return Json(new {isValid=true,html="" });
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return Json(new { isValid = true, html = "" });
+        }
+
+        // //POST: Students/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Student student = db.Students.Find(id);
+        //    db.Students.Remove(student);
+        //    db.SaveChanges();
+        //    return Json(new { isValid = true, html = "" });
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
